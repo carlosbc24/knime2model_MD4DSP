d3a92c5f620164578e6df470cde0b82569062824
carlosbc24 <cbc@unex.es>
2025-05-06 13:37:09 +0200

    added 01 Data Cleaning Workflow 2-1 generated code

--- input_KNIME_workflows/paper_workflows/01_Data_Cleaning_Workflow2-1/generated_code/contracts_Job_01_Data_Cleaning_Workflow2_1.py
+++ input_KNIME_workflows/paper_workflows/01_Data_Cleaning_Workflow2-1/generated_code/contracts_Job_01_Data_Cleaning_Workflow2_1.py
@@ -11,27 +11,41 @@ from functions.PMML import PMMLModel
 
 def generateWorkflow():
 	#-----------------New DataProcessing-----------------
-	columnFilter_Country_Region_Year_Infant_deaths_Under_five_deaths_Adult_mortality_Alcohol_consumption_Hepatitis_B_Measles_BMI_Polio_Diphtheria_Incidents_HIV_GDP_per_capita_Population_mln_Schooling_Economy_status_Developed_Economy_status_Developing_Life_expectancy__input_dataDictionary_df=pd.read_parquet('/wf_validation_python/data/output/columnFilter_input_dataDictionary.parquet')
+	columnFilter_Thinness_ten_nineteen_years_Thinness_five_nine_years__input_dataDictionary_df=pd.read_parquet('/wf_validation_python/data/output/columnFilter_input_dataDictionary.parquet')
 
 	if os.path.exists('/wf_validation_python/data/output/columnFilter_output_dataDictionary.parquet'):
-		columnFilter_Country_Region_Year_Infant_deaths_Under_five_deaths_Adult_mortality_Alcohol_consumption_Hepatitis_B_Measles_BMI_Polio_Diphtheria_Incidents_HIV_GDP_per_capita_Population_mln_Schooling_Economy_status_Developed_Economy_status_Developing_Life_expectancy__output_dataDictionary_df=pd.read_parquet('/wf_validation_python/data/output/columnFilter_output_dataDictionary.parquet')
+		columnFilter_Thinness_ten_nineteen_years_Thinness_five_nine_years__output_dataDictionary_df=pd.read_parquet('/wf_validation_python/data/output/columnFilter_output_dataDictionary.parquet')
 
-	field_list_columnFilter_PRE_field_range=['Country', 'Region', 'Year', 'Infant_deaths', 'Under_five_deaths', 'Adult_mortality', 'Alcohol_consumption', 'Hepatitis_B', 'Measles', 'BMI', 'Polio', 'Diphtheria', 'Incidents_HIV', 'GDP_per_capita', 'Population_mln', 'Schooling', 'Economy_status_Developed', 'Economy_status_Developing', 'Life_expectancy']
+	field_list_columnFilter_PRE_field_range=['Thinness_ten_nineteen_years', 'Thinness_five_nine_years']
 	if contract_pre_post.check_field_range(fields=field_list_columnFilter_PRE_field_range,
-								data_dictionary=columnFilter_Country_Region_Year_Infant_deaths_Under_five_deaths_Adult_mortality_Alcohol_consumption_Hepatitis_B_Measles_BMI_Polio_Diphtheria_Incidents_HIV_GDP_per_capita_Population_mln_Schooling_Economy_status_Developed_Economy_status_Developing_Life_expectancy__input_dataDictionary_df,
+								data_dictionary=columnFilter_Thinness_ten_nineteen_years_Thinness_five_nine_years__input_dataDictionary_df,
 								belong_op=Belong(0)):
-		print('PRECONDITION columnFilter(Country, Region, Year, Infant_deaths, Under_five_deaths, Adult_mortality, Alcohol_consumption, Hepatitis_B, Measles, BMI, Polio, Diphtheria, Incidents_HIV, GDP_per_capita, Population_mln, Schooling, Economy_status_Developed, Economy_status_Developing, Life_expectancy)_PRE_fieldRange VALIDATED')
+		print('PRECONDITION columnFilter(Thinness_ten_nineteen_years, Thinness_five_nine_years)_PRE_fieldRange VALIDATED')
 	else:
-		print('PRECONDITION columnFilter(Country, Region, Year, Infant_deaths, Under_five_deaths, Adult_mortality, Alcohol_consumption, Hepatitis_B, Measles, BMI, Polio, Diphtheria, Incidents_HIV, GDP_per_capita, Population_mln, Schooling, Economy_status_Developed, Economy_status_Developing, Life_expectancy)_PRE_fieldRange NOT VALIDATED')
+		print('PRECONDITION columnFilter(Thinness_ten_nineteen_years, Thinness_five_nine_years)_PRE_fieldRange NOT VALIDATED')
 	
 	
-	field_list_columnFilter_POST_field_range=['Country', 'Region', 'Year', 'Infant_deaths', 'Under_five_deaths', 'Adult_mortality', 'Alcohol_consumption', 'Hepatitis_B', 'Measles', 'BMI', 'Polio', 'Diphtheria', 'Incidents_HIV', 'GDP_per_capita', 'Population_mln', 'Schooling', 'Economy_status_Developed', 'Economy_status_Developing', 'Life_expectancy']
+	field_list_columnFilter_POST_field_range=['Thinness_ten_nineteen_years', 'Thinness_five_nine_years']
 	if contract_pre_post.check_field_range(fields=field_list_columnFilter_POST_field_range,
-								data_dictionary=columnFilter_Country_Region_Year_Infant_deaths_Under_five_deaths_Adult_mortality_Alcohol_consumption_Hepatitis_B_Measles_BMI_Polio_Diphtheria_Incidents_HIV_GDP_per_capita_Population_mln_Schooling_Economy_status_Developed_Economy_status_Developing_Life_expectancy__output_dataDictionary_df,
-								belong_op=Belong(0)):
-		print('POSTCONDITION columnFilter(Country, Region, Year, Infant_deaths, Under_five_deaths, Adult_mortality, Alcohol_consumption, Hepatitis_B, Measles, BMI, Polio, Diphtheria, Incidents_HIV, GDP_per_capita, Population_mln, Schooling, Economy_status_Developed, Economy_status_Developing, Life_expectancy)_POST_fieldRange VALIDATED')
+								data_dictionary=columnFilter_Thinness_ten_nineteen_years_Thinness_five_nine_years__output_dataDictionary_df,
+								belong_op=Belong(1)):
+		print('POSTCONDITION columnFilter(Thinness_ten_nineteen_years, Thinness_five_nine_years)_POST_fieldRange VALIDATED')
 	else:
-		print('POSTCONDITION columnFilter(Country, Region, Year, Infant_deaths, Under_five_deaths, Adult_mortality, Alcohol_consumption, Hepatitis_B, Measles, BMI, Polio, Diphtheria, Incidents_HIV, GDP_per_capita, Population_mln, Schooling, Economy_status_Developed, Economy_status_Developing, Life_expectancy)_POST_fieldRange NOT VALIDATED')
+		print('POSTCONDITION columnFilter(Thinness_ten_nineteen_years, Thinness_five_nine_years)_POST_fieldRange NOT VALIDATED')
+	
+	
+	columns_list_columnFilter_Thinness_ten_nineteen_years_Thinness_five_nine_years__INV_condition = ['Thinness_ten_nineteen_years', 'Thinness_five_nine_years']
+	
+	if contract_invariants.check_inv_filter_columns(data_dictionary_in=columnFilter_Thinness_ten_nineteen_years_Thinness_five_nine_years__input_dataDictionary_df,
+							data_dictionary_out=columnFilter_Thinness_ten_nineteen_years_Thinness_five_nine_years__output_dataDictionary_df,
+							columns=columns_list_columnFilter_Thinness_ten_nineteen_years_Thinness_five_nine_years__INV_condition,
+							belong_op=Belong(0)):
+		print('INVARIANT columnFilter(Thinness_ten_nineteen_years, Thinness_five_nine_years)_INV_condition VALIDATED')
+	else:
+		print('INVARIANT columnFilter(Thinness_ten_nineteen_years, Thinness_five_nine_years)_INV_condition NOT VALIDATED')
+	
+	
+	
 	
 	
 set_logger("contracts")
