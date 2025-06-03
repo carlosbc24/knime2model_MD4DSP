c62689688ce1725a02d518475f5623bde2f9959e
CARLOS BREUER CARRASCO <cbc@unex.es>
2025-06-04 00:14:25 +0200

    updated generated code for subworkflow 02 Data Cleaning 3-2-2

--- input_KNIME_workflows/paper_workflows/02_Data_Cleaning_Workflow3-2-2/generated_code/contracts_Job_02_Data_Cleaning_Workflow3_2_2.py
+++ input_KNIME_workflows/paper_workflows/02_Data_Cleaning_Workflow3-2-2/generated_code/contracts_Job_02_Data_Cleaning_Workflow3_2_2.py
@@ -16,37 +16,20 @@ def generateWorkflow():
 	if os.path.exists('/wf_validation_python/data/output/join_output_dataDictionary.parquet'):
 		join_Name_with_City__output_dataDictionary_df=pd.read_parquet('/wf_validation_python/data/output/join_output_dataDictionary.parquet')
 
-	if contract_invariants.check_inv_missing_value_missing_value(data_dictionary_in=join_Name_with_City__input_dataDictionary_df,
-											data_dictionary_out=join_Name_with_City__output_dataDictionary_df,
-											belong_op_in=Belong(1), belong_op_out=Belong(1),
-											field_in = 'Name', field_out='Name with City', origin_function="String Manipulation"):
-		print('INVARIANT String Manipulation(Name) BelongOpIn:NotBelong BelongOpOut:NotBelong VALIDATED')
-	else:
-		print('INVARIANT String Manipulation(Name) BelongOpIn:NotBelong BelongOpOut:NotBelong NOT VALIDATED')
-	if contract_invariants.check_inv_missing_value_missing_value(data_dictionary_in=join_Name_with_City__input_dataDictionary_df,
-											data_dictionary_out=join_Name_with_City__output_dataDictionary_df,
-											belong_op_in=Belong(1), belong_op_out=Belong(1),
-											field_in = 'City', field_out='Name with City', origin_function="String Manipulation"):
-		print('INVARIANT String Manipulation(City) BelongOpIn:NotBelong BelongOpOut:NotBelong VALIDATED')
-	else:
-		print('INVARIANT String Manipulation(City) BelongOpIn:NotBelong BelongOpOut:NotBelong NOT VALIDATED')
 	
-	
-	
-	
-	dictionary_join_specialValue_INV_THEN={'Name': True, ' - ': False, 'City': True}
+	dictionary_join_check_INV_THEN={'Name': True, ' - ': False, 'City': True}
 	if contract_invariants.check_inv_join(data_dictionary_in=join_Name_with_City__input_dataDictionary_df,
 								data_dictionary_out=join_Name_with_City__output_dataDictionary_df,
-								dictionary=dictionary_join_specialValue_INV_THEN,
+								dictionary=dictionary_join_check_INV_THEN,
 								field_out='Name with City', origin_function="String Manipulation"):
 		print('INVARIANT String Manipulation(Name) JoinInputs:Name VALIDATED')
 	else:
 		print('INVARIANT String Manipulation(Name) JoinInputs:Name NOT VALIDATED')
 	
-	dictionary_join_specialValue_INV_THEN={'Name': True, ' - ': False, 'City': True}
+	dictionary_join_check_INV_THEN={'Name': True, ' - ': False, 'City': True}
 	if contract_invariants.check_inv_join(data_dictionary_in=join_Name_with_City__input_dataDictionary_df,
 								data_dictionary_out=join_Name_with_City__output_dataDictionary_df,
-								dictionary=dictionary_join_specialValue_INV_THEN,
+								dictionary=dictionary_join_check_INV_THEN,
 								field_out='Name with City', origin_function="String Manipulation"):
 		print('INVARIANT String Manipulation(City) JoinInputs:Name VALIDATED')
 	else:
